ee0d53d3546a914fee15f9688ce15f087323a18b
Akshay <149981521+akbhard@users.noreply.github.com>
2025-03-09 18:24:57 -0400

    Added some notes on how to restructure my Javascript, going to play around with using functions on click for getting numbers and operators.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let numbers = []
 let operator = "/"
 
+// In order to get the numbers, based on some youtube video, I should use an operator on click for both getNumber and getOperator.
+// And then push that into the operate function itself.
+
 const operate = function (numbers, operator) {
     if (operator === "+") {
         return (numbers[0] + numbers[1]).toFixed(2)
