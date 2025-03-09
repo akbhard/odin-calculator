40c00a2db8cea0cc397f8c8a2418b7c52d73ce34
Akshay <149981521+akbhard@users.noreply.github.com>
2025-03-09 18:27:04 -0400

    Rewrote operate function to arrow function

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let numbers = []
+let numbers = [42,7]
 let operator = "/"
 
 // In order to get the numbers, based on some youtube video, I should use an operator on click for both getNumber and getOperator.
 // And then push that into the operate function itself.
 
-const operate = function (numbers, operator) {
+const operate = (numbers, operator) => {
     if (operator === "+") {
         return (numbers[0] + numbers[1]).toFixed(2)
     } else if (operator === "-") {
