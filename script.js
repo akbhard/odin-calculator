e90cdd12e6f19820d6b8c1d77b9c759bb3065b44
Akshay <149981521+akbhard@users.noreply.github.com>
2025-03-10 21:53:22 -0400

    Added link to script.js in HTML file. Added in functions for getNumber, getOperator, and clear.

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-let numbers = [42,7]
-let operator = "/"
+let numberOne = ""
+let numberTwo = ""
+let numbers = [numberOne, numberTwo]
+let operator = []
+let step = 0
 
 // In order to get the numbers, based on some youtube video, I should use an operator on click for both getNumber and getOperator.
 // And then push that into the operate function itself.
 
-const operate = (numbers, operator) => {
+const getNumber = (number) => {
+    if (step === 0) {
+        number = number.toString()
+        numberOne += number
+        numbers[0] = Number(numberOne)
+    } if (step === 1) {
+        number = number.toString()
+        numberTwo += number
+        numbers[1] = Number(numberTwo)
+    }
+    return numbers
+}
+
+const getOperator = (op) => {
+    step += 1
+    return operator = op
+}
+
+const operate = () => {
     if (operator === "+") {
         return (numbers[0] + numbers[1]).toFixed(2)
     } else if (operator === "-") {
@@ -16,4 +37,27 @@ const operate = (numbers, operator) => {
     }
 }
 
-console.log(operate(numbers, operator))
+const clear = () => {
+    numberOne = ""
+    numberTwo = ""
+    numbers = []
+    step = 0
+    operator = []
+}
+
+// getNumber(9)
+// getNumber(7)
+// getOperator("/")
+// getNumber(1)
+// console.log(numbers)
+// console.log(operate())
+// clear()
+// console.log(numbers)
+// console.log(step)
+// getNumber(4)
+// getNumber(2)
+// getNumber(2)
+// getOperator("*")
+// getNumber(3)
+// console.log(numbers)
+// console.log(operate())
